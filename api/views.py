e016632b2109a64c64f13f49a7e98d0095a475e0
franckalainai <franckalain.ai@gmail.com>
2021-08-11 09:12:06 +0000

    custom method for create and update rating

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
+from rest_framework.serializers import Serializer
 from api.models import Movie
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
 
 # custom methods
 from rest_framework.response import Response
@@ -13,16 +15,28 @@ class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
 
     # custom methods
+    # create or update rating
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
             # user = request.user
-            user = 
-            print('user', user)
-            response = {'message': 'its working'}
-            return Response(response, status=status.HTTP_200_OK)
+            user = User.objects.get(id=1)
+            
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
         else:
             response = {'message': 'you need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
